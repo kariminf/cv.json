6a27217bbf510083f4e8c069df3206528c62e26f
Abdelkrime Aries <kariminfo0@gmail.com>
2016-09-17 20:10:24 +0100

    first essay

---
+++ jsonvcard.js
@@ -0,0 +1,70 @@
+(function(){
+
+  document.addEventListener('DOMContentLoaded', function () {
+    init();
+  });
+
+  function init(){
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", "./portfelio.json", true);
+
+    rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4 && rawFile.status == "200") {
+        process(rawFile.responseText);
+      }
+    }
+    rawFile.send(null);
+  }
+
+  function process(json){
+    var data = JSON.parse(json);
+
+    document.title = data.name + " " + data.family;
+
+    var rawFile = new XMLHttpRequest();
+    addStyleSheet(data.style + "style.css");
+    var file = data.style + "template.htm";
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4 && rawFile.status == "200") {
+        process2(data, rawFile.responseText);
+      }
+    }
+    rawFile.send(null);
+
+  }
+
+  function process2(data, template){
+    var result = template;
+    for (var e in data){
+      result = result.replace("%" + e + "%", data[e]);
+    }
+    document.body.innerHTML = result;
+  }
+
+
+  function embed(html){
+    document.body.innerHTML += html;
+  }
+
+  function addStyleSheet(url){
+    var cssId = 'myCss';  // you could encode the css path itself to generate id..
+    if (!document.getElementById(cssId))
+    {
+      var head  = document.getElementsByTagName('head')[0];
+      var link  = document.createElement('link');
+      link.id   = cssId;
+      link.rel  = 'stylesheet';
+      link.type = 'text/css';
+      link.href = url;
+      link.media = 'all';
+      head.appendChild(link);
+    }
+  }
+
+
+
+
+
+}());
