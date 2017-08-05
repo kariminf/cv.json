61b802a1bc3ffeb9f0342cfed5c331b6e0f777f0
Abdelkrime Aries <kariminfo0@gmail.com>
2017-08-05 11:29:44 +0100

    generate yui docs again

---
+++ docs/docs/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "JsonVCard"
+    ],
+    "modules": [
+        "jsonvcard"
+    ],
+    "allModules": [
+        {
+            "displayName": "jsonvcard",
+            "name": "jsonvcard",
+            "description": "Provides the scripts to generate a CV"
+        }
+    ],
+    "elements": []
+} };
+});
